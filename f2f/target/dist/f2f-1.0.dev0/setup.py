1738ba77c6e48eaf0855323991fb629fe483d408
Karan Inder Singh <34513461+karaninder@users.noreply.github.com>
2019-04-12 01:08:25 +0530

    virtual environment

---
+++ f2f/target/dist/f2f-1.0.dev0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+class install(_install):
+    def pre_install_script(self):
+        pass
+
+    def post_install_script(self):
+        pass
+
+    def run(self):
+        self.pre_install_script()
+
+        _install.run(self)
+
+        self.post_install_script()
+
+if __name__ == '__main__':
+    setup(
+        name = 'f2f',
+        version = '1.0.dev0',
+        description = '',
+        long_description = '',
+        author = '',
+        author_email = '',
+        license = '',
+        url = '',
+        scripts = [],
+        packages = [],
+        namespace_packages = [],
+        py_modules = [],
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python'
+        ],
+        entry_points = {},
+        data_files = [],
+        package_data = {},
+        install_requires = [],
+        dependency_links = [],
+        zip_safe = True,
+        cmdclass = {'install': install},
+        keywords = '',
+        python_requires = '',
+        obsoletes = [],
+    )
